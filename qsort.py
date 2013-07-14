b1122ea3342b88e3637fc7782d9674e6ba9366fa
Osada Paranaliyanage <osadalakmal@gmail.com>
2013-07-14 23:59:20 +0100

    Adding pivot selection

--- qsort.py
+++ qsort.py
@@ -2,30 +2,60 @@
 import sys
 import copy
 
-def getPivot(start,end):
-    """ Return the pivot point of a given array"""
-    return int((start + end)/2)
+def getMedian(num1, num2, num3):
+    if (num1 < num2):
+        if (num2 < num3):
+            return num2
+        elif num1 < num3:
+            return num3
+        else:
+            return num1
+    else:
+        if (num3 > num1):
+            return num1
+        elif (num3 > num2):
+            return num3
+        else:
+            return num2
 
-def inplace_qsort(input,start,end):
+def getPivot(start, end,pivotPos):
+    """ Return the pivot point of a given array"""
+    return {
+        "start" : lambda start, end: start,
+        "end" : lambda start, end: end,
+        "med" : lambda start, end: getMedian(start, end, int((start + end + 1)/2)),
+    }.get(pivotPos)(start=start, end=end)
+
+def inplace_qsort(input, start, end, pivotPos):
     """sorts a given array in "input" and changes the array inplace"""
     if (start == end):
-        return
-    i = start + 1
-    pivotIndex = start
+        return 0
+    comp = (end - start)
+    pivotIndex = getPivot(start, end, pivotPos)
     pivot = input[pivotIndex]
+    i = start
     for j in range(i,end + 1):
-        if input[j] < pivot:
+        if (j == pivotIndex):
+            continue
+        elif input[j] < pivot:
             temp = input[j]
             input[j] = input[i]
             input[i] = temp
             i += 1
-    temp = input[i-1]
-    input[i-1] = pivot
+    temp = input[i]
+    input[i] = pivot
     input[pivotIndex] = temp
-    if (i-2 >= start):
-        inplace_qsort(input,start,i-2)
-    if (i <= end):
-        inplace_qsort(input,i,end)
+    if (pivotPos == "start"):
+        if (i-2 > start):
+            comp += inplace_qsort(input, start, i-2, pivotPos)
+        if (i <= end):
+            comp += inplace_qsort(input, i, end, pivotPos)
+    elif (pivotPos == "end"):
+        if (i > start):
+            comp += inplace_qsort(input, start, (i-1), pivotPos)
+        if (i < end):
+            comp += inplace_qsort(input, (i+1), end, pivotPos)
+    return comp
 
 def easy_qsort(input,start,end,result):
     """This routine sorts the input array in "input" list and stores the
@@ -65,8 +95,9 @@ def main():
     if (sys.argv[2] == "easy"):
         easy_qsort(array,0,len(array)-1,result)
     else:
-        inplace_qsort(array,0,len(array)-1)
+        comp = inplace_qsort(array, 0, len(array)-1, sys.argv[2])
         result = array
+    print("number of comparisons is %d" % (comp))
     for num in result:
         print(num)
 
